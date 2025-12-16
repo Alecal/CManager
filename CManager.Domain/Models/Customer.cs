5f02356861778b5daf8084c688fce0178e25ad66
Alexander Nyman <stalander@gmail.com>
2025-12-16 14:05:20 +0100

    Gjort meny för konsolapplikation

--- CManager.Domain/Models/Customer.cs
+++ CManager.Domain/Models/Customer.cs
@@ -7,5 +7,5 @@ public class Customer
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
-    public string Address { get; set; } = null!;
+    public Adress Address { get; set; } = null!;
 }
--- CManager.Presentation.ConsoleApp/Program.cs
+++ CManager.Presentation.ConsoleApp/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+int selectedItem = 0;
+string[] menuItems = new string[]
+{
+    "Create new customer",
+    "View all customers",
+    "Quit application"
+};
+
+bool navigatingMenu = true;
+
+while (navigatingMenu)
+{
+    Console.ResetColor();
+    Console.Clear();
+    Console.WriteLine("┌────────────────────── MENU ─────────────────────────┐");
+    Console.WriteLine("│                                                     │");
+    Console.WriteLine("│ Use arrowkeys to navigate and choose an option:     │");
+    Console.WriteLine("│                                                     │");
+
+    for (int i = 0; i < menuItems.Length; i++)
+    {
+        if (i == selectedItem)
+        {
+            Console.Write("│ > ");
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.Write($" {menuItems[i]} ");
+
+            Console.ResetColor();
+
+            int remainingSpace = 48 - menuItems[i].Length;
+            Console.WriteLine(new string(' ', remainingSpace) + "│");
+
+        }
+        else
+        {
+            Console.WriteLine($"│  {menuItems[i].PadRight(50)} │");
+        }
+    }
+
+    Console.WriteLine("│                                                     │");
+    Console.WriteLine("└─────────────────────────────────────────────────────┘");
+
+
+    var key = Console.ReadKey(true).Key;
+
+    if (key == ConsoleKey.UpArrow)
+    { selectedItem = (selectedItem == 0) ? menuItems.Length - 1 : selectedItem - 1;}
+    else if
+        (key == ConsoleKey.DownArrow)
+    { selectedItem = (selectedItem == menuItems.Length - 1) ? 0 : selectedItem + 1; }
+    else if
+        (key == ConsoleKey.Enter)
+    {
+        if (selectedItem == 0)
+        {
+            Console.WriteLine("Navigating to: Create Customer...");
+            Console.ReadKey();
+        }
+        else if (selectedItem == 1)
+        {
+            Console.WriteLine("Navigating to: View All...");
+            Console.ReadKey();
+        }
+        else if (selectedItem == 2)
+        {
+            navigatingMenu = false;
+        }
+    }
+}
